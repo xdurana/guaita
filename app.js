8b2e4d5de8a46ff2623fa4e4c017dc0e8be807fe
Xavier Duran <xdurana@uoc.edu>
2015-02-02 10:42:32 +0100

    GUAITA-307

--- app.js
+++ app.js
@@ -35,29 +35,28 @@ require('./lib/controllers/widget/index')(app, config);
 require('./lib/controllers/monitor/index')(app, config);
 
 
-//var cluster = require('cluster');
-//var numCPUs = require('os').cpus().length;
-//
-//if (cluster.isMaster) {
-//    // Fork workers.
-//    for (var i = 0; i < numCPUs; i++) {
-//        cluster.fork();
-//    }
-//    cluster.on('exit', function(worker, code, signal) {
-//        console.log('worker ' + worker.process.pid + ' died');
-//    });
-//} else {
-//    // Workers can share any TCP connection
-//    // In this case its a HTTP server
-//    var server = app.listen(app.get('port'), function () {
-//        config.log('Express server listening on port ' + app.get('port'));
-//    });
-//}
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 
-var server = app.listen(app.get('port'), function () {
-    config.log('Express server listening on port ' + app.get('port'));
-});
+if (cluster.isMaster) {
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('worker ' + worker.process.pid + ' died');
+    });
+} else {
+    // Workers can share any TCP connection
+    // In this case its a HTTP server
+    var server = app.listen(app.get('port'), function () {
+        config.log('Express server listening on port ' + app.get('port'));
+    });
+}
 
+//var server = app.listen(app.get('port'), function () {
+//    config.log('Express server listening on port ' + app.get('port'));
+//});
 
 var gracefulShutdown = function() {
     config.log("Received kill signal, shutting down gracefully.");
